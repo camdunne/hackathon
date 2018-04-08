ca7c9e5b267ebc68320daf089e578b5e8f74bc34
Cameron <camdunne@gmail.com>
2018-04-08 12:23:02 -0400

    clean up code styling and variables

--- client/popup.js
+++ client/popup.js
@@ -1,7 +1,5 @@
-(function(){
-  var data = {
-    columns: [ 'data1', 0]
-  };
+(function() {
+
 var container = document.querySelector('div.container');
 var search = container.querySelector('div.search');
 var button = document.querySelector('button');
@@ -11,34 +9,35 @@ function clickHandler(event) {
   var from = document.querySelector('input.fromDate');
   var to = document.querySelector('input.toDate');
   var fromDate = from && from.value;
-  var startTime = (fromDate) ? new Date(fromDate).getTime(): undefined;
+  var startTime = fromDate && new Date(fromDate).getTime();
 
   var toDate = to && to.value;
   var endTime = (toDate) ? new Date(toDate).getTime(): new Date().getTime();
   var url = document.querySelector('input.urlText');
   var text = url && url.value;
 
-  var options = { 
+  var options = {
     text,
     startTime,
     endTime,
   };
 
   function searchHandler(values) {
-    var url = Object.keys(values).map((key, i) => {
-      return values[key].url;
-    });
-    var options = { 
+    var urls = Object.keys(values).map((key) => values[key].url);
+    var options = {
       method: 'POST',
-      body: JSON.stringify({ url }),
+      body: JSON.stringify({ urls }),
      };
+
     fetch('http://localhost:8888/send_urls', options)
       .then(res => res.json())
-      .then(({ data }) => {
-        var columns = Object.keys(data).map((key, i) => [ key, data[key]]);
-        var sorted = [... columns].sort((a,b) => {
-
+      .then((data) => {
+        var columnsUnsorted = Object.keys(data).map((key) => [ key, data[key]]);
+        var sorted = [...columns].sort((a, b) => {
+          return a[1] - b[1];
         });
+        var columns = sorted.slice(4);
+        chart.unload();
         chart.load({
           columns,
         })
@@ -54,10 +53,7 @@ button.addEventListener('click', clickHandler);
 
 var chart = c3.generate({
   data: {
-      // iris data from R
       columns: [
-          // ['data1', 30],
-          // ['data2', 120],
       ],
       type : 'pie',
       onclick: function (d, i) { console.log("onclick", d, i); },
@@ -66,25 +62,5 @@ var chart = c3.generate({
   }
 });
 
-setTimeout(function () {
-  chart.load({
-      columns: [
-          ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
-          ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
-          ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
-      ]
-  });
-}, 1500);
-
-// setTimeout(function () {
-//   chart.unload({
-//       ids: 'data1'
-//   });
-//   chart.unload({
-//       ids: 'data2'
-//   });
-// }, 2500);
-
 
 })();
-
