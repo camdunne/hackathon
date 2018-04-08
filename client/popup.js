fd735291f1820c5d3daa66bab32e884606ec6331
Cameron <camdunne@gmail.com>
2018-04-07 20:07:36 -0400

    cameron - chart template

--- client/popup.js
+++ client/popup.js
@@ -1,20 +1,69 @@
 (function(){
 
-var container = window.document.getElementsByClassName('container');
+var container = document.querySelector('div.container');
+var search = container.querySelector('div.search');
 var button = document.querySelector('button');
-console.log('container', container)
-button.addEventListener('click', clickHandler);
 
-function clickHandler() {
+
+function clickHandler(event) {
+console.log(event)
+var from = document.querySelector('input.fromDate');
+var to = document.querySelector('input.toDate');
+  var fromDate = from && from.value;
+  var startTime = (fromDate) ? new Date(fromDate).getTime(): undefined;
+
+  var toDate = to && to.value;
+  var endTime = (toDate) ? new Date(toDate).getTime(): new Date().getTime();
+  console.log(startTime, endTime);
+
   var options = { 
-    text: ""
+    text: '',
+    startTime,
+    endTime,
   };
-
   chrome.history.search(options, function(values){
-    console.log('values', values);
+    var data = Object.keys(values).map((key, i) => {
+      return values[key].url;
+    });
+    console.log('values', data);
     
   });
 }
+
+button.addEventListener('click', clickHandler);
+
+var chart = c3.generate({
+  data: {
+      // iris data from R
+      columns: [
+          ['data1', 30],
+          ['data2', 120],
+      ],
+      type : 'pie',
+      onclick: function (d, i) { console.log("onclick", d, i); },
+      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+      onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+  }
+});
+
+setTimeout(function () {
+  chart.load({
+      columns: [
+          ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+          ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+          ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+      ]
+  });
+}, 1500);
+
+setTimeout(function () {
+  chart.unload({
+      ids: 'data1'
+  });
+  chart.unload({
+      ids: 'data2'
+  });
+}, 2500);
 
 })();
 
